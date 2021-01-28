baadb5e4cba9cdfdab711190c9f61b3d2461e86c
Andrew Kraus <KalkalovichIvan@yandex.ru>
2021-01-28 22:47:28 +0300

    fix

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ const NotFoundErr = require('./errors/NotFoundErr');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+});
+
 
 mongoose.connect('mongodb://localhost:27017/backendkraus', {
   useNewUrlParser: true,
