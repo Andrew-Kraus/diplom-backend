efd35d4f9784821037da2d2e190a798984ba2ae9
Andrew Kraus <KalkalovichIvan@yandex.ru>
2021-01-28 23:29:50 +0300

    fix

--- app.js
+++ app.js
@@ -2,27 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const corsOptions = {
-  origin: [
-    'http://localhost:8080',
-    'https://andrew-kraus.github.io/',
-    'https://andrew-kraus.github.io/diplom-frontend/',
-  ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: [
-    'Content-Type',
-    'origin',
-    'Authorization',
-    'x-access-token',
-    'accept',
-    'Access-Control-Allow-Origin',
-  ],
-  credentials: true,
-};
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -32,7 +11,6 @@ const NotFoundErr = require('./errors/NotFoundErr');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
